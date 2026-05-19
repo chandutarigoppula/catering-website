25b3f955686634fcfa78124000e3948abebdf82c
chandutarigoppula <chandutarigoppula@users.noreply.github.com>
2026-05-19 13:25:23 +0530

    Deploy site to gh-pages branch

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import SriVenkateshwaraCatering from "./App.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <SriVenkateshwaraCatering />
-  </React.StrictMode>
-);
